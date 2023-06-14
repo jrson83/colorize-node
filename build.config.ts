763d4b7d411a1beb0234801e4894ce3f510e4e96
Jrson <45284565+jrson83@users.noreply.github.com>
2023-06-15 00:27:45 +0200

    chore: cleanup

--- build.config.ts
+++ build.config.ts
@@ -10,17 +10,4 @@ export default defineBuildConfig({
       minify: true,
     },
   },
-  /* https://github.com/unjs/unbuild/issues/112#issuecomment-1331769536 */
-  hooks: {
-    'rollup:dts:options'(_ctx, options) {
-      if (Array.isArray(options.plugins)) {
-        const esbuildPlugin = options.plugins.find(
-          // rome-ignore lint/suspicious/noExplicitAny: <explanation>
-          (p) => (p as any).name === 'esbuild'
-        )
-        // rome-ignore lint/suspicious/noExplicitAny: <explanation>
-        ;(esbuildPlugin as any).renderChunk = () => null
-      }
-    },
-  },
 })
